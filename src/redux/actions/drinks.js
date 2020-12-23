8526f686e2dc9cfab60399533990bd9d2177e58a
artemijsS <artjom1408@gmail.com>
2020-12-23 23:23:58 +0200

    log del

--- src/redux/actions/drinks.js
+++ src/redux/actions/drinks.js
@@ -19,7 +19,6 @@ export const fetchDrinks = (category, sortBy) => (dispatch) => {
 
     axios.get(`http://localhost:1337/products?type_of_product.name=drinks&${category !== null ? `categories.name=${category}` : ''}&_sort=${sortBy.type}:${sortBy.order}`)
         .then(({ data }) => {
-            console.log(data)
             dispatch(setDrinks(data));
         });
 }
