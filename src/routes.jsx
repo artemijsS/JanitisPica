4ce9e1f782b0d0a9f049d2a2352bef55f6e4a956
artemijsS <artjom1408@gmail.com>
2021-01-01 21:23:15 +0200

    backend on heroku

--- src/routes.jsx
+++ src/routes.jsx
@@ -19,7 +19,7 @@ export const useRoutes = () => {
             </Route>
             <Route path="/admin" exact>
                 {
-                    <a id="test" href="http://localhost:1337/admin" style={{margin:"auto"}}>test</a>
+                    <a id="test" href="https://young-ridge-27848.herokuapp.com/admin" style={{margin:"auto"}}>test</a>
                 }
             </Route>
 
